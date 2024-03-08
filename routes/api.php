bf6f7482c3249338661942eb90ccb50fe349a637
luizpilan1004 <luizsenai1004@hotmail.com>
2024-03-08 10:16:22 -0300

    exercicios 1

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/nome\idade',function(Request $request){
+    $nome = $request ->input('nome');
+    $idade = $request ->input('idade');
+    return 'Meu Nome é' . $nome . ' Minha Idade é '. $idade;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
