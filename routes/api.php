d306b806768219dcdda190141e6970f1ab0bb692
luizpilan1004 <luizsenai1004@hotmail.com>
2024-03-08 10:28:21 -0300

    exercicios 1 esqueci

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+
+Route::get('/nome',function(Request $request){
+    $nome = $request ->input('nome');
+return $nome;
+});
+
+
 Route::get('/nome\idade',function(Request $request){
     $nome = $request ->input('nome');
     $idade = $request ->input('idade');
